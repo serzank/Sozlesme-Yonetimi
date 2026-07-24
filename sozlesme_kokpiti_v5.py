3e06f5a105101b44f66ff9762e9dae76d1332304
serzank <48967911+serzank@users.noreply.github.com>
2026-07-24 15:24:26 +0300

    sozlesme_kokpiti_v5.py

--- sozlesme_kokpiti_v5.py
+++ sozlesme_kokpiti_v5.py
@@ -581,7 +581,7 @@ d_key = f"{start_date}_{end_date}"
 # --- VERİ KÖPRÜSÜ ---
 # --- VERİ KÖPRÜSÜ ---
 with st.spinner("PNX Veritabanlarına Bağlanıyor..."):
-    tufe_sheet_url = "https://docs.google.com/spreadsheets/d/15tHPO39U5ltgMDQVa4jEb5b2sHCWV1KuHq5epNrWNh0/edit?gid=0#gid=0"
+    tufe_sheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS1f-Ix-2hu_Gcuc0_dxZ-BcZbvoHPPewNmQAkOrC6H1nKOR_TYaAtyhWO2KObVArIvus0l1AvrsLY9/pub?gid=0&single=true&output=csv"
     
     # TÜFE Google Sheet'ten, ÜFE EVDS'den çekiliyor
     tufe_res = get_sheets_tufe_data(tufe_sheet_url, start_date, end_date)
